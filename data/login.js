887030f219983004913f9ebe5ef0c94b7647a24c
MdTasib <ohidulalam442@gmail.com>
2022-10-16 12:18:51 +0600

    job model

---
+++ data/login.js
@@ -0,0 +1,30 @@
+/*
+{
+"email": "test@test.com",
+    "password": "test123@#ABC",
+    "confirmPassword": "test123@#ABC",
+    "firstName": "test",
+    "lastName": "user",
+    "shippingAddress": "944 osthir Street",
+    "presentAddress": "944 osthir Street",
+    "permanentAddress": "944 Russell Street",
+    "imageURL": ""
+}
+//for manager
+/*
+"name":"Manager",
+"email":"managerctg@test.com",
+"password":"tasib123456##",
+"confirmPassword":"tasib123456##",
+"firtsName":"Manager of",
+"lastName":"CTG",
+"contactNumber":"11111111111",
+"shippingAddress:"944 osthir Street",
+"division":"chattogram",
+"imageURL":"",
+"status":"active",
+"emergencyContactNumber":"01712345678",
+"presentAddress":"944 osthir Street",
+"permanentAddress":"944 Russell Street",
+"nationalIdImageURL":"",
+*/
---
+++ model/Job.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const jobSchema = mongoose.Schema(
+	{
+		email: {
+			type: String,
+			validate: [validator.isEmail, "Provide a valid Email"],
+			trim: true,
+			lowercase: true,
+		},
+		title: {
+			type: String,
+			required: [true, "Please provide a title for this product."],
+			trim: true,
+			unique: [true, "Product must be unique"],
+			lowercase: true,
+			minLenght: [3, "title must be al last 3 characters."],
+			maxLenght: [100, "title is too large"],
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: {
+				values: ["full-time", "part-time", "project-base"],
+				message: "status can't be {VALUE}",
+			},
+		},
+		salary: {
+			type: Number,
+			required: true,
+			min: [0, "Job price can't be nagetive"],
+		},
+		location: {
+			type: String,
+			required: [true, "Please provide your job location"],
+		},
+		role: {
+			type: String,
+			enum: ["hr", "candidate", "admin"],
+			default: "hr",
+		},
+		applyURL: {
+			type: String,
+			validate: [validator.isURL, "Please provide a valid url"],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
